9c1989aca85900aeea8763f367c26464b3df0f3c
Maleesha Dewmini <87409233+it21020308dewmini@users.noreply.github.com>
2021-07-18 16:27:53 +0000

    complete

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main() {
+  int n1,n2;
+  float avg=0;
+  printf("Number 1 =");
+  scanf("%d",&n1);
+  printf("Number 2 =");
+  scanf("%d",&n2);
+ avg=(n1+n2)/2.0;
+ printf("Average =%.2f",avg);
+
   
   return 0;
 }
